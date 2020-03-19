726ffa265caba561fd020a30e72f3679950edfbd
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-03-18 23:50:31 -0500

    login, logout, private routes

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,4 +1,5 @@
 const usersCtrl = {};
+const passport = require("passport");
 
 const User = require("../models/User");
 
@@ -47,8 +48,16 @@ usersCtrl.renderSigninForm = (req, res) => {
   res.render("users/signin");
 };
 
-usersCtrl.signin = (req, res) => {};
+usersCtrl.signin = passport.authenticate("local", {
+  failureRedirect: "/users/signin",
+  successRedirect: "/notes",
+  failureFlash: true
+});
 
-usersCtrl.logout = (req, res) => {};
+usersCtrl.logout = (req, res) => {
+  req.logout();
+  req.flash("success_msg", "You are logged out now");
+  res.redirect("/users/signin");
+};
 
 module.exports = usersCtrl;
